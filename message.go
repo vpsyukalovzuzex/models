add63c32ac7a1a0157d4a13291c7463156b5ae6e
Vladimir Psyukalov <vpsyukalov@zuzex.com>
2025-01-29 18:59:33 +0300

    Add error to message

--- message.go
+++ message.go
@@ -4,5 +4,13 @@ type Message struct {
 	UserId   string `json:"user_id"`
 	Function string `json:"function"`
 
+	Error Error `json:"error"`
+
 	Data []byte `json:"data"`
+}
+
+type Error struct {
+	Code int `json:"code"`
+
+	Message string `json:"message"`
 }
